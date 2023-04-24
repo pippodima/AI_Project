7566a883951d14ce578a00e87a38879db794f70d
pippodima <pippodima99@gmail.com>
2023-04-24 21:48:06 +0200

    small improvement

--- Models.py
+++ Models.py
@@ -1,18 +1,29 @@
 import numpy as np
 class Bernoulli:
     def __int__(self):
-        self.prior = []
-        self.likelihood = []
+        self.prior = None
+        self.likelihood = None
 
     def train(self, X, y):
-        X = np.where(X > 0, 1, 0)
+        X_ones = np.where(X > 0, 1, 0)
         n_documents, vocab_size = np.shape(X)
         classes = np.unique(y)
 
         for i, c in enumerate(classes):
-            X_c = X[y == c]
+            X_c = X_ones[y == c]
             self.prior.append(X_c.shape[0] / n_documents)
             self.likelihood.append((X_c.sum(axis=0) + 1) / (X_c.shape[0] + 2))
 
+        self.likelihood = np.reshape((len(classes), vocab_size))
+        self.prior = np.reshape((len(classes), 1))
+
     def test(self, X):
+        X_ones = np.where(X > 0, 1, 0)
+        n_documents, vocab_size = np.shape(X)
+
+        self.likelihood = (X_ones * self.likelihood) + (1-X_ones) * (1-self.likelihood)
+
+
+
+
         print('ciao')  # todo capire come legare eq 2 e 3 del pdf
